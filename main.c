f008fd9cb5f7a8c4aaac41b5cc3d6a65baa3ca7f
MOUAD EL ALLI <mouadalli311@gmail.com>
2025-09-02 15:39:40 +0100

    file uploaded

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ 
+    // exercice 3******
+    // int number,i;
+    // int result = 0;
+    // printf("entre a number: ");
+    // scanf("%d", &number);
+
+    // for (i=1; i<=number; i++){
+
+    //     result += i;
+    // };
+    // printf("the result is %d", result);
+
+
+    // EXERCCIE 4:*******8
+
+    // int number,i; 
+    // int current_odd = 1;
+    // // int result = 0;
+    // printf("entre a number: ");
+    // scanf("%d", &number);
+  
+
+       
+    // for (i = 0; i < number; i++) {
+
+
+//   // exercice 5
+//     int number,i; 
+//     // int current_odd = 1;
+//     int exposant ;
+//     int result = 1;  
+//     printf("entre a the base number: ");
+//     scanf("%d", &number);
+
+//     printf("enter the exposant ");
+//     scanf("%d" , &exposant);
+
+
+//     for (i = 0; i < exposant; i++) {
+//             result *= number;  
+//         }
+
+//         printf("The result is: %d\n", result);
+    
+     // exercice 6
+    
+
+    //  exercice 5
+    int number,i; 
+    // int current_odd = 1;
+    int exposant ;
+    int result = 1;  
+    printf("entre a the base number: ");
+    scanf("%d", &number);
+    
+    for 
+     
+
+    return 0;
+}
